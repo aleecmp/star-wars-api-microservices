48b985fe3ece77a10f60731b2c807a0fe4cd84dd
Alexis Campusano <ale.cmpb@gmail.com>
2023-10-04 22:23:43 -0300

    feat: add proxy configuration for planets and films microservices

--- gateway/index.js
+++ gateway/index.js
@@ -7,12 +7,20 @@ const app = express();
 app.use(morgan("dev"));
 
 app.use("/characters", createProxyMiddleware({
-    target: "http://localhost:8001",
+    target: "http://characters:8001",
+    changeOrigin: true,
+}));
+
+app.use("/characters", createProxyMiddleware({
+    target: "http://films:8002",
+    changeOrigin: true,
+}));
+
+app.use("/characters", createProxyMiddleware({
+    target: "http://planets:8003",
     changeOrigin: true,
 }));
 
 app.listen(8000, () => {
     console.log("Gateway on port 8000");
 });
-
-
